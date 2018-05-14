d95168d263a0edbb0a458c4827d92e48209261ba
tech201823 <tech201823@gmail.com>
2018-05-14 13:52:40 +0900

    case whenを使った練習

--- pra.rb
+++ pra.rb
@@ -1,11 +1,19 @@
-a = 20
-b = 15
-c = a+b
-d =35
-if c == d
-    puts "true"
-else
-    puts "false"
-end
+# a = 20
+# b = 15
+# c = a+b
+# d =35
+# if c == d
+#     puts "true"
+# else
+#     puts "false"
+# end
 
-# test
+a = 11
+case a
+when 0..5
+    puts "5以下"
+when 6..10
+    puts "10以下"
+else
+    puts "10以上"
+end
